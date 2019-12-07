add1b55cd5c78c8731c469186fee3a4b7c041db7
Francesco Pagani Galvao <francescogalvao@gmail.com>
2019-12-07 18:29:44 -0400

    Update index.blade.php

--- resources/views/jogos/index.blade.php
+++ resources/views/jogos/index.blade.php
@@ -39,7 +39,7 @@
       <footer class="mastfoot mt-auto">
         <div class="inner">
           <p>Site desenvolvido por <a href="https://github.com/franpgn">@franpgn</a> e <a href="https://github.com/Sanderson910">@sanderson910</a></p>
-          <p>Link para o repositório no GitHub: <a href=""></a></p>
+          <p>Link para o repositório no GitHub: <a href="https://github.com/franpgn/FranquiaX/">Franquia X</a></p>
         </div>
       </footer>
     </div>
